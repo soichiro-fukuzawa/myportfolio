e2f18d83567eaf8b93ba162d5afebaa77bd64df0
soichiro-fukuzawa <67686070+soichiro-fukuzawa@users.noreply.github.com>
2020-07-06 12:53:52 +0900

    Update db_path.php

--- db_path.php
+++ db_path.php
@@ -1,6 +1,6 @@
 <?php
 try{
-    $dbh = new PDO("pgsql:host=localhost;port=5432;dbname=postgres;user=postgres;password=10063603");
+    $dbh = new PDO("pgsql:host=ec2-34-233-226-84.compute-1.amazonaws.com;port=5432;dbname=d90rkgo8o06k76;user=vnnylgdrblxhof;password=59881c24b1377df395b0b758de86d22e4f8398e98e03f5ed0c7e4dc4ab86dd2b");
 }catch(PDOException $e){
     echo "データベースとの接続に失敗しました。";
 }
